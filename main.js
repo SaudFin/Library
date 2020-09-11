af1c09503a3a36f0e83d320d5083a652f58e572c
SaudFin <developer.saud@gmail.com>
2020-09-11 18:48:21 +0300

    add new book button now is fully functional with direct rendreing

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-let library = [
-  { title: "hello World", author: "Blum Blah", pages: 320, read: "YES" },
-  { title: "hello Moon", author: "Blum Blah", pages: 500, read: "NO" },
-  { title: "hello World", author: "Blum blooh", pages: 242, read: "YES" }
-];
+let library = [];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -15,42 +11,74 @@ function Book(title, author, pages, read) {
   };
 }
 // ***** Needs to be changed latter to take input from a form
-function addBookToLibrary() {
-  let title = prompt("What is the title of the book?");
-  let author = prompt("who is the author of the book?");
-  let pages = prompt("How many pages are in the book?");
-  let read = prompt("Did you read it?");
-  library.push(new Book(title, author, pages, read));
+function addBookToLibrary(title, author, pages, read) {
+  let newBook = new Book(title, author, pages, read);
+  library.push(newBook);
+  displayBooks();
 }
 
 // to display Books
 function displayBooks() {
   let container = document.getElementById("container");
-  library.forEach(book => {
-    let titleDiv = document.createElement("div");
-    titleDiv.className = "title";
-    titleDiv.innerHTML = book.title;
-    let authorDiv = document.createElement("div");
-    authorDiv.className = "author";
-    authorDiv.innerHTML = book.author;
-    let pagesDiv = document.createElement("div");
-    pagesDiv.className = "pages";
-    pagesDiv.innerHTML = book.pages;
-    let readDiv = document.createElement("div");
-    readDiv.className = "read";
-    readDiv.innerHTML = book.read;
-    container.appendChild(titleDiv);
-    container.appendChild(authorDiv);
-    container.appendChild(pagesDiv);
-    container.appendChild(readDiv);
-    // container.style.gridTemplateRows = `repeat(${library.length}, 1fr 1fr 1fr 1fr)`;
-  });
+  let book = library[library.length - 1];
+  // library.forEach(book => {
+  let titleDiv = document.createElement("div");
+  titleDiv.className = "title";
+  titleDiv.innerHTML = book.title;
+  let authorDiv = document.createElement("div");
+  authorDiv.className = "author";
+  authorDiv.innerHTML = book.author;
+  let pagesDiv = document.createElement("div");
+  pagesDiv.className = "pages";
+  pagesDiv.innerHTML = book.pages;
+  let readDiv = document.createElement("div");
+  readDiv.className = "read";
+  readDiv.innerHTML = book.read;
+  container.appendChild(titleDiv);
+  container.appendChild(authorDiv);
+  container.appendChild(pagesDiv);
+  container.appendChild(readDiv);
+  // container.style.gridTemplateRows = `repeat(${library.length}, 1fr 1fr 1fr 1fr)`;
+  // });
 }
 
 let addBooksButton = document.getElementById("addBook");
+let turnClick = false;
 
+function showHideForm() {
+  console.log("one");
+  if (turnClick === false) {
+    document.querySelector("form").style.visibility = "visible";
+    document.getElementById("addBook").style.visibility = "hidden";
+    turnClick = true;
+  } else {
+    document.querySelector("form").style.visibility = "hidden";
+    document.getElementById("addBook").style.visibility = "visible";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").value = "";
+    turnClick = false;
+  }
+}
 addBooksButton.addEventListener("click", () => {
-  document.querySelector("form").style.visibility = "visible";
+  showHideForm();
 });
-// addBookToLibrary();
-displayBooks();
+
+function sumbitBook() {
+  console.log("here");
+  let title = document.getElementById("title").value;
+  console.log(title);
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.getElementById("read").value;
+  addBookToLibrary(title, author, pages, read);
+  showHideForm();
+}
+
+let additionActionButton = document.getElementById("additionAction");
+
+additionActionButton.addEventListener("click", () => {
+  sumbitBook();
+});
+
+// displayBooks();
